d51719f65b30841a4ef4a2e99cd9b6f8d527aa53
Guillaume Rose <guillaume.rose@gmail.com>
2010-08-06 22:09:49 +0200

    Init

---
+++ bin/named/timedb.c
@@ -0,0 +1,232 @@
+/*
+ * Copyright (C) 2004, 2007  Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (C) 2000, 2001  Internet Software Consortium.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND ISC DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS.  IN NO EVENT SHALL ISC BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+ * OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/* $Id: timedb.c,v 1.10 2007/06/19 23:47:10 tbox Exp $ */
+
+/*
+ * A simple database driver that enables the server to return the
+ * current time in a DNS record.
+ */
+
+#include <config.h>
+
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
+
+#include <isc/print.h>
+#include <isc/result.h>
+#include <isc/util.h>
+
+#include <dns/sdb.h>
+
+#include <named/globals.h>
+
+#include "timedb.h"
+#include "mongo.h"
+
+#define	MONGO_STRING_LENGTH	8196
+
+#define MONGO_HOST		"127.0.0.1"
+#define MONGO_PORT		27017
+#define MONGO_BASE		"production.users"
+
+#define MONGO_SEARCH_FIELD	"dns"
+#define MONGO_MAC_FIELD		"dns"
+#define MONGO_DHCP_FIELD	"ip"
+
+static dns_sdbimplementation_t *timedb = NULL;
+
+mongo_connection conn[1];
+mongo_connection_options opts;
+
+char *mongo_host = MONGO_HOST,
+     *mongo_base = MONGO_BASE,
+     *mongo_mac_field = MONGO_MAC_FIELD,
+     *mongo_dhcp_field = MONGO_DHCP_FIELD;
+
+int mongo_port = MONGO_PORT;
+
+int
+mongo_start() 
+{	
+	strncpy(opts.host, mongo_host, 255);
+	opts.host[254] = '\0';
+	opts.port = mongo_port;
+
+	if (mongo_connect(conn, &opts)){
+		printf("Failed to connect\n");
+		return 0;
+	}
+	
+	printf("Connected to MongoDB\n");
+	return 1;
+}
+
+void 
+find_in_array(bson_iterator *it, char *key_ref, char *value_ref, char *key_needed, char *value_needed) 
+{
+	char value_ref_found[MONGO_STRING_LENGTH];
+	char value_needed_found[MONGO_STRING_LENGTH];
+
+	bson_iterator i;
+	
+	while(bson_iterator_next(it)) {
+		switch(bson_iterator_type(it)){
+			case bson_string:
+				if (strcmp(bson_iterator_key(it), key_ref) == 0)
+					strcpy(value_ref_found, bson_iterator_string(it));
+				if (strcmp(bson_iterator_key(it), key_needed) == 0)
+					strcpy(value_needed_found, bson_iterator_string(it));
+				break;
+			case bson_object:
+			case bson_array:
+				bson_iterator_init(&i, bson_iterator_value(it));
+				find_in_array(&i, key_ref, value_ref, key_needed, value_needed);
+				break;
+			default:
+				break;
+		}
+	}
+	
+	if (strcmp(value_ref_found, value_ref) == 0)
+		strcpy(value_needed, value_needed_found);
+}
+
+int 
+find_dhcp_options(char *mac, char *dhcp) 
+{
+	bson_buffer bb;
+	
+	bson query;
+	bson field;
+	bson result;
+	
+	bson_buffer_init(&bb);
+	bson_append_string(&bb, MONGO_SEARCH_FIELD, mac);
+
+	bson_append_finish_object(&bb);
+	bson_from_buffer(&query, &bb);
+	
+	bson_empty(&field);
+
+	bson_empty(&result);
+
+	MONGO_TRY{
+		if (mongo_find_one(conn, MONGO_BASE, &query, &field, &result) == 0) {
+			return 0;
+		}
+	}MONGO_CATCH{
+		mongo_start();
+		return 0;
+	}
+	
+	bson_iterator it;
+	bson_iterator_init(&it, result.data);
+	
+	find_in_array(&it, MONGO_MAC_FIELD, mac, MONGO_DHCP_FIELD, dhcp);
+	return 1;
+}
+
+/*
+ * This database operates on relative names.
+ *
+ * "time" and "@" return the time in a TXT record.  
+ * "clock" is a CNAME to "time"
+ * "current" is a DNAME to "@" (try time.current.time)
+ */ 
+isc_result_t
+timedb_lookup(const char *zone, const char *name, void *dbdata,
+	      dns_sdblookup_t *lookup)
+{
+	isc_result_t result;
+
+	UNUSED(zone);
+	UNUSED(dbdata);
+	
+	char option_buffer[MONGO_STRING_LENGTH] = "";
+	find_dhcp_options(name, option_buffer);
+	
+	if (strcmp(option_buffer, "") != 0) {
+		printf("Entrée DNS trouvée pour %s : %s\n", name, option_buffer);
+		result = dns_sdb_putrr(lookup, "A", 86400, option_buffer);
+		if (result != ISC_R_SUCCESS)
+			return (ISC_R_FAILURE);
+	} else {
+		return (ISC_R_NOTFOUND);
+	}
+
+	return (ISC_R_SUCCESS);
+}
+
+/*
+ * lookup() does not return SOA or NS records, so authority() must be defined.
+ */
+static isc_result_t
+timedb_authority(const char *zone, void *dbdata, dns_sdblookup_t *lookup) {
+	isc_result_t result;
+
+	UNUSED(zone);
+	UNUSED(dbdata);
+
+	result = dns_sdb_putsoa(lookup, "localhost.", "root.localhost.", 2010080201); //  YYYYMMDDXX
+	if (result != ISC_R_SUCCESS)
+		return (ISC_R_FAILURE);
+
+	result = dns_sdb_putrr(lookup, "ns", 86400, "ns1.localdomain.");
+	if (result != ISC_R_SUCCESS)
+		return (ISC_R_FAILURE);
+	result = dns_sdb_putrr(lookup, "ns", 86400, "ns2.localdomain.");
+	if (result != ISC_R_SUCCESS)
+		return (ISC_R_FAILURE);
+
+	return (ISC_R_SUCCESS);
+}
+
+/*
+ * This zone does not support zone transfer, so allnodes() is NULL.  There
+ * is no database specific data, so create() and destroy() are NULL.
+ */
+static dns_sdbmethods_t timedb_methods = {
+	timedb_lookup,
+	timedb_authority,
+	NULL,	/* allnodes */
+	NULL,	/* create */
+	NULL	/* destroy */
+};
+
+/*
+ * Wrapper around dns_sdb_register().
+ */
+isc_result_t
+timedb_init(void) {
+	mongo_start();
+
+	unsigned int flags;
+	flags = DNS_SDBFLAG_RELATIVEOWNER | DNS_SDBFLAG_RELATIVERDATA;
+	return (dns_sdb_register("time", &timedb_methods, NULL, flags,
+				 ns_g_mctx, &timedb));
+}
+
+/*
+ * Wrapper around dns_sdb_unregister().
+ */
+void
+timedb_clear(void) {
+	if (timedb != NULL)
+		dns_sdb_unregister(&timedb);
+}
